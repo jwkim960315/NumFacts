4c18599d329046c09c224d302686c6208050606a
jwkim960315 <jkim11@wesleyan.edu>
2018-12-21 23:36:40 +0900

    Mixed-Contents Issue Cannot be solved

--- src/apis/numbers.js
+++ src/apis/numbers.js
@@ -3,9 +3,7 @@ import http from 'http';
 
 export default axios
 	.create({
-		baseURL: 'http://numbersapi.com',
-		withCredentials: false,
-		httpAgent: new http.Agent({ keepAlive: true })
+		baseURL: 'https://numbersapi.com'
 	});
 
 
