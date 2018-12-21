19480b4f32987a5d43ee20a27b3bb8d162f92db6
jwkim960315 <jkim11@wesleyan.edu>
2018-12-21 23:09:48 +0900

    Working on API mixed-content issue

--- src/apis/numbers.js
+++ src/apis/numbers.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import http from 'http';
 
 export default axios
 	.create({
 		baseURL: 'http://numbersapi.com',
-		withCredentials: false
+		withCredentials: false,
+		httpAgent: new http.Agent({ keepAlive: true })
 	});
 
 
