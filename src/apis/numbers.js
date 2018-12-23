5c3f31e81b758661bb47a5818b02b3afcf28b7c7
jwkim960315 <jkim11@wesleyan.edu>
2018-12-24 01:27:02 +0900

    Working on fixing CORS issue

--- src/apis/numbers.js
+++ src/apis/numbers.js
@@ -2,13 +2,7 @@ import axios from 'axios';
 
 export default axios
 	.create({
-		baseURL: 'http://numbersapi.com',
-		// headers: {
-		// 	'Access-Control-Allow-Origin': '*',
-		// 	"Access-Control-Allow-Methods": "GET,POST",
-		// 	'Access-Control-Allow-Headers': 'Content-Type'
-		// }
-		crossdomain: true
+		baseURL: `${'https://cors-anywhere.herokuapp.com/'}http://numbersapi.com`
 	});
 
 
