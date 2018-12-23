c70815aa7c8857940e57e9c955dc5877ff81d074
jwkim960315 <jkim11@wesleyan.edu>
2018-12-24 00:03:21 +0900

    Deploying to heroku

--- src/apis/numbers.js
+++ src/apis/numbers.js
@@ -4,7 +4,9 @@ export default axios
 	.create({
 		baseURL: 'http://numbersapi.com',
 		headers: {
-			'Access-Control-Allow-Origin' : '*'
+			'Access-Control-Allow-Origin': 'https://num-fact.herokuapp.com/',
+			"Access-Control-Allow-Headers": "X-Requested-With",
+			'Access-Control-Allow-Headers': 'Content-Type'
 		}
 	});
 
